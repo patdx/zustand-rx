64acc6d08e13c346f226583f735d45e2d5a8f468
Patrick Miller <pm@Patricks-Air.miller-family>
2021-11-09 02:29:53 +0900

    initial version

---
+++ libs/zustand-rx/README.md
@@ -0,0 +1,7 @@
+# zustand-rx
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test zustand-rx` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/zustand-rx/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'zustand-rx',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/zustand-rx',
+};
