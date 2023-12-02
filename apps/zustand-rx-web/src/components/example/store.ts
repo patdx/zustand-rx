e31eb1c280cfd20a4ab29f5d6a0de68750ad8ccc
Patrick Miller <pamiller.pdx@gmail.com>
2023-12-02 18:51:28 +0000

    fix: fix types when used with zustand immer middleware
    fix #953

--- apps/zustand-rx-web/src/components/example/store.ts
+++ apps/zustand-rx-web/src/components/example/store.ts
@@ -3,12 +3,24 @@
 import { toStream } from 'zustand-rx';
 import { combine } from 'zustand/middleware';
 import { createStore } from 'zustand/vanilla';
+import { immer } from 'zustand/middleware/immer';
 
 export const store = createStore(
   combine({ bears: 0 }, (set) => ({
     increment: () => set((state) => ({ bears: state.bears + 1 })),
-  }))
+  })),
 );
+
+export const immerStore = createStore(
+  immer(
+    combine({ bears: 0 }, (set) => ({
+      increment: () => set((state) => ({ bears: state.bears + 1 })),
+    })),
+  ),
+);
+
+// Test to make sure immer store works too
+toStream(immerStore, (store) => store.bears);
 
 /* Observable<number> */
 export const bears$ = toStream(store, (state) => state.bears, {
