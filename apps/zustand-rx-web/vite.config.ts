25aa08ffb5ae8b02aadb7ff854bc1c4ae4fe1793
Patrick Miller <pamiller.pdx@gmail.com>
2024-01-26 02:31:59 +0900

    chore: update nx and husky

--- apps/zustand-rx-web/vite.config.ts
+++ apps/zustand-rx-web/vite.config.ts
@@ -4,6 +4,14 @@ import { defineConfig } from 'vite';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
+  root: __dirname,
+  build: {
+    outDir: '../../dist/apps/zustand-rx-web',
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
   cacheDir: '../../node_modules/.vite/zustand-rx-web',
 
   plugins: [nxViteTsPaths()],
@@ -14,6 +22,11 @@ export default defineConfig({
   // },
 
   test: {
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../coverage/apps/zustand-rx-web',
+      provider: 'v8',
+    },
     globals: true,
     cache: {
       dir: '../../node_modules/.vitest',
